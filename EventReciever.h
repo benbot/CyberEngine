6acbe7d24d3ede0beea0fb7e6223738efffa763e
dartosgamer <dartosgamer@gmail.com>
2012-11-11 14:34:07 -0500

    Added the game class and modified the main.cpp

---
+++ EventReciever.h
@@ -0,0 +1,14 @@
+#ifndef EVENT_H_INCLUDED
+#define EVENT_H_INCLUDED
+
+namespace game
+{
+
+    class EventReciever //: public irr::IEventReceiver
+    {
+    public:
+        //virtual bool OnEvent(irr::SEvent& event);
+    };
+}
+
+#endif
---
+++ Game.cpp
@@ -0,0 +1,12 @@
+#include "Game.h"
+
+namespace game
+{
+    Game::Game() {}
+    Game::~Game() {}
+
+    int Game::init(int x = 800, int y = 600)
+    {
+
+    }
+}
