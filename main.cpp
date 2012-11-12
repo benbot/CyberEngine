87fcf84057103c88d988080b48c40979f0ef7959
dartos <dartosgamer@gmail.com>
2012-11-11 21:57:21 -0500

    removed weird HEAD things in some of the files

--- main.cpp
+++ main.cpp
@@ -13,23 +13,17 @@ int main()
     device->getFileSystem()->addFileArchive("///home/dartosgamer/Downloads/map-losttemple.pk3");
 
     scene::IAnimatedMesh* mesh = mgr->getMesh("losttemple.bsp");
-<<<<<<< HEAD
+
     scene::ISceneNode* node = mgr->addMeshSceneNode(mesh->getMesh(0), 0, 0);
-=======
-    scene::ISceneNode* node = 0;
 
     node = mgr->addOctreeSceneNode(mesh->getMesh(0), 0, 0);
->>>>>>> baf385c66d7faba0bd26d441f5be0b09b3c061fa
+
     mgr->addCameraSceneNodeFPS();
 
     while(device->run())
     {
         driver->beginScene(true, true, video::SColor(255, 0, 0, 0));
         mgr->drawAll();
-<<<<<<< HEAD
-=======
-       // std::cout << device->getTimer()->getTime()/1000 << std::endl;
->>>>>>> baf385c66d7faba0bd26d441f5be0b09b3c061fa
         driver->endScene();
     }
 
