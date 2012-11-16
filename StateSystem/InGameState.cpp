0b7df56dac154793bc8e0b408f0c18f409787861
dartos <dartosgamer@gmail.com>
2012-11-16 00:50:38 -0500

    Nightly Commit

--- StateSystem/InGameState.cpp
+++ StateSystem/InGameState.cpp
@@ -7,8 +7,7 @@ namespace game
             device->getFileSystem()->addFileArchive("///home/dartosgamer/Documents/Projects/CyberEngine/map-20kdm2.pk3");
             scene::IAnimatedMesh* mesh = sceneManager->getMesh("20kdm2.bsp");
             sceneManager->addCameraSceneNodeFPS();
-            scene::ISceneNode* node = sceneManager->addMeshSceneNode(mesh->getMesh(0));
-            node->setMaterialFlag(video::EMF_LIGHTING, false);
+            scene::ISceneNode* node = sceneManager->addOctreeSceneNode(mesh->getMesh(0));
     }
 
     InGameState::~InGameState() {}
@@ -22,7 +21,7 @@ namespace game
 
     states InGameState::update ()
     {
-        draw(true, true, video::SColor(255, 0, 0, 0));
+        draw(true, true, video::SColor(255, 60, 20, 200));
         return S_CONTINUE;
     }
 }
